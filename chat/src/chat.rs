fc021dc25513a84d022020ed1b1e8671e7bdbb15
smallbraingames <smallbraindev@gmail.com>
2024-10-01 16:34:03 -0700

    feat(chat): text input

--- chat/src/chat.rs
+++ chat/src/chat.rs
@@ -1,15 +1,20 @@
-use gpui::{div, rgb, IntoElement, Length, ParentElement, Pixels, Render, Styled, ViewContext};
+mod input;
 
 use crate::logic::Logic;
+use gpui::{div, rgb, IntoElement, Length, ParentElement, Pixels, Render, Styled, ViewContext};
+use gpui::{View, VisualContext};
+use input::TextInput;
 
 pub struct Chat {
     logic: Logic,
+    message_input: View<TextInput>,
 }
 
 impl Chat {
     pub fn new(cx: &mut ViewContext<Self>) -> Self {
         let logic = Logic::new();
         logic.start_test_task();
+
         let mut message_watch = logic.get_message_watch();
         cx.spawn(|view, mut cx| async move {
             while let Ok(_) = message_watch.changed().await {
@@ -21,7 +26,13 @@ impl Chat {
             }
         })
         .detach();
-        Self { logic }
+
+        let message_input = cx.new_view(|cx| TextInput::new(cx));
+
+        Self {
+            logic,
+            message_input,
+        }
     }
 }
 
@@ -29,7 +40,7 @@ impl Render for Chat {
     fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
         let message = self.logic.get_latest_message();
         div()
-            .flex()
+            .flex_col()
             .bg(rgb(0x2e7d32))
             .size(Length::Definite(Pixels(300.0).into()))
             .justify_center()
@@ -40,5 +51,6 @@ impl Render for Chat {
             .text_xl()
             .text_color(rgb(0xffffff))
             .child(format!("latest message: {}", &message))
+            .child(self.message_input.clone())
     }
 }
