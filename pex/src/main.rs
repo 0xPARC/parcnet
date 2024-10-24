572fdee09bb3f0505e691e4da3649d6cc43408c2
ludens <ludens@lattice.xyz>
2024-10-23 19:30:29 -0700

    we can create pods now

--- pex/src/main.rs
+++ pex/src/main.rs
@@ -4,44 +4,45 @@ use std::{
 };
 
 use eyre::{eyre, Result};
-use pex::{self, Entry, Env, MyPods, Pod, User, Value};
+use pex::{self, Env, MyPods, User, Value};
 
 use plonky2::field::goldilocks_field::GoldilocksField;
 #[tokio::main]
 async fn main() -> Result<()> {
-    let script = r#"[add
-        [from alice [pod? x]]
-        [max
-            [from bob [pod? y]]
-            100]]
-    "#;
-    let shared = Arc::new(Mutex::new(HashMap::new()));
+    // let script = r#"[add
+    //     [from alice [pod? x]]
+    //     [max
+    //         [from bob [pod? y]]
+    //         100]]
+    // "#;
+    // let shared = Arc::new(Mutex::new(HashMap::new()));
 
-    let alice = tokio::spawn(pex::eval(
-        script,
-        Env::new(
-            User::from("alice"),
-            shared.clone(),
-            pex::my_pods![{"x" => 22}],
-        ),
-    ));
+    // let alice = tokio::spawn(pex::eval(
+    //     script,
+    //     Env::new(
+    //         User::from("alice"),
+    //         shared.clone(),
+    //         pex::my_pods![{"x" => 22}],
+    //     ),
+    // ));
 
-    let bob = tokio::spawn(pex::eval(
-        script,
-        Env::new(
-            User::from("bob"),
-            shared.clone(),
-            pex::my_pods![{"y" => 20}],
-        ),
-    ));
+    // let bob = tokio::spawn(pex::eval(
+    //     script,
+    //     Env::new(
+    //         User::from("bob"),
+    //         shared.clone(),
+    //         pex::my_pods![{"y" => 20}],
+    //     ),
+    // ));
 
-    match tokio::try_join!(flatten(alice), flatten(bob)) {
-        Ok((alice, bob)) => {
-            println!("alice: {:?} bob: {:?}", alice, bob);
-            Ok(())
-        }
-        Err(e) => Err(e),
-    }
+    // match tokio::try_join!(flatten(alice), flatten(bob)) {
+    //     Ok((alice, bob)) => {
+    //         println!("alice: {:?} bob: {:?}", alice, bob);
+    //         Ok(())
+    //     }
+    //     Err(e) => Err(e),
+    // }
+    Ok(())
 }
 
 async fn flatten<T>(handle: tokio::task::JoinHandle<Result<T>>) -> Result<T> {
