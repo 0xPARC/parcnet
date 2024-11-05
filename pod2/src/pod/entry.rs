269177285864b3e27f870774081d506144b5df8f
Ahmad <root@ahmadafuni.com>
2024-11-05 18:18:36 +1000

    Pad SchnorrPOD vector entries

--- pod2/src/pod/entry.rs
+++ pod2/src/pod/entry.rs
@@ -1,3 +1,4 @@
+use anyhow::{anyhow, Result};
 use plonky2::field::goldilocks_field::GoldilocksField;
 
 use super::value::ScalarOrVec;
@@ -22,6 +23,34 @@ impl Entry {
         Entry {
             key: key.to_string(),
             value: ScalarOrVec::Vector(value),
+        }
+    }
+
+    /// Pads an entry's value if it is a vector. Padding is chosen so
+    /// as to define the same set as the original vector.
+    pub fn pad_if_vec<const VL: usize>(&self) -> Result<Self> {
+        match &self.value {
+            ScalarOrVec::Scalar(_) => Ok(self.clone()),
+            ScalarOrVec::Vector(v) if v.len() <= VL => v
+                .get(0)
+                .ok_or(anyhow!(
+                    "Entry with key {} has empty vector as value!",
+                    self.key
+                ))
+                .and_then(|padding| {
+                    Ok(Self {
+                        key: self.key.clone(),
+                        value: ScalarOrVec::Vector(
+                            [v.clone(), (v.len()..VL).map(|_| *padding).collect()].concat(),
+                        ),
+                    })
+                }),
+            ScalarOrVec::Vector(v) => Err(anyhow!(
+                "Entry with key {} has vector of length {}, which exceeds the maximum ({})!",
+                self.key,
+                v.len(),
+                VL
+            )),
         }
     }
 
