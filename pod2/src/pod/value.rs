546d0f0281db322709b30444720dd90fd5859c37
Ahmad <root@ahmadafuni.com>
2024-11-09 14:41:57 +1000

    Basic POD1 introduction

--- pod2/src/pod/value.rs
+++ pod2/src/pod/value.rs
@@ -1,3 +1,4 @@
+use parcnet_pod::pod::pod_impl::{string_hash, PodValue};
 use plonky2::{
     field::goldilocks_field::GoldilocksField,
     hash::poseidon::PoseidonHash,
@@ -27,6 +28,31 @@ impl HashableEntryValue for Vec<GoldilocksField> {
 pub enum ScalarOrVec {
     Scalar(GoldilocksField),
     Vector(Vec<GoldilocksField>),
+}
+
+impl From<&PodValue> for ScalarOrVec {
+    fn from(pod_value: &PodValue) -> Self {
+        match pod_value {
+            PodValue::Int(n) => Self::Scalar(GoldilocksField(*n as u64)),
+            PodValue::Cryptographic(n) => Self::Vector(
+                n.to_u32_digits()
+                    .1
+                    .iter()
+                    .rev()
+                    .map(|n| GoldilocksField(*n as u64))
+                    .collect(),
+            ),
+            PodValue::String(s) => Self::Vector(
+                string_hash(s)
+                    .to_u32_digits()
+                    .1
+                    .iter()
+                    .rev()
+                    .map(|n| GoldilocksField(*n as u64))
+                    .collect(),
+            ),
+        }
+    }
 }
 
 impl HashableEntryValue for ScalarOrVec {
