2ad0f91ecfafabf71a116a7d7edbced9b339ad3e
Ahmad <root@ahmadafuni.com>
2024-11-11 14:08:33 +1000

    POD1 refactor

--- parcnet-pod/src/crypto/lean_imt.rs
+++ parcnet-pod/src/crypto/lean_imt.rs
@@ -1,7 +1,9 @@
-use crate::crypto::poseidon_hash::hash_bigints;
-use num_bigint::BigInt;
+use babyjubjub_ark::Fq;
+use poseidon_ark::Poseidon;
 
-pub fn lean_poseidon_imt(inputs: &[BigInt]) -> Result<BigInt, &'static str> {
+pub fn lean_poseidon_imt(inputs: &[Fq]) -> Result<Fq, &'static str> {
+    let poseidon = Poseidon::new();
+    
     if inputs.is_empty() {
         return Err("At least one input is required");
     }
@@ -12,7 +14,7 @@ pub fn lean_poseidon_imt(inputs: &[BigInt]) -> Result<BigInt, &'static str> {
         let mut new_items = Vec::new();
         for chunk in items.chunks(2) {
             if chunk.len() == 2 {
-                let hash = hash_bigints(&[chunk[0].clone(), chunk[1].clone()])?;
+                let hash = poseidon.hash(vec![chunk[0].clone(), chunk[1].clone()]).map_err(|_| "Error hashing")?;
                 new_items.push(hash);
             } else {
                 new_items.push(chunk[0].clone());
@@ -21,46 +23,46 @@ pub fn lean_poseidon_imt(inputs: &[BigInt]) -> Result<BigInt, &'static str> {
         items = new_items;
     }
 
-    Ok(items[0].clone())
+    Ok(items[0])
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use num_bigint::BigInt;
     use std::str::FromStr;
+    use babyjubjub_ark::Fq;
 
+    type Error = Box<dyn std::error::Error>;
+    
     #[test]
-    fn test_lean_imt() {
-        let input_strs = ["1", "2", "3", "4", "5"];
-        let inputs: Vec<BigInt> = input_strs
-            .iter()
-            .map(|s| BigInt::from_str(s).unwrap())
-            .collect();
+    fn test_lean_imt() -> Result<(), Error> {
+        let inputs = ["1", "2", "3", "4", "5"].into_iter().map(|s| Fq::from_str(s)).collect::<Result<Vec<_>, ()>>().map_err(|_| "Error converting strings to fields.")?;
 
         let result = lean_poseidon_imt(&inputs);
         assert!(result.is_ok());
         let hash = result.unwrap();
         assert_eq!(
             hash,
-            BigInt::from_str(
+            Fq::from_str(
                 "11512324111804726054755717642058292259866309947044530224809882918003853859592"
             )
             .expect("can't parse")
         );
+
+        Ok(())
     }
 
     #[test]
     fn test_lean_imt_single_input() {
-        let inputs = vec![BigInt::from(42)];
+        let inputs = vec![Fq::from(42)];
         let result = lean_poseidon_imt(&inputs);
         assert!(result.is_ok());
-        assert_eq!(result.unwrap(), BigInt::from(42));
+        assert_eq!(result.unwrap(), Fq::from(42));
     }
 
     #[test]
     fn test_lean_imt_empty_input() {
-        let inputs: Vec<BigInt> = vec![];
+        let inputs: Vec<Fq> = vec![];
         let result = lean_poseidon_imt(&inputs);
         assert!(result.is_err());
         assert_eq!(result.unwrap_err(), "At least one input is required");
