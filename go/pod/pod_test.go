b419f6bb38e3bce1434c51c68dea702b60078061
Richard Liu <richard.y.liu@berkeley.edu>
2025-01-24 22:19:19 -0800

    slight refactor / cleanup -- need to fix verify

--- go/pod/pod_test.go
+++ go/pod/pod_test.go
@@ -12,34 +12,6 @@ import (
 	"github.com/iden3/go-iden3-crypto/v2/poseidon"
 )
 
-// func TestCreatePod(t *testing.T) {
-// 	p, j, err := CreatePod(
-// 		"0001020304050607080900010203040506070809000102030405060708090001",
-// 		map[string]interface{}{
-// 			"hello":           map[string]interface{}{"string": "world"},
-// 			"year":            map[string]interface{}{"int": 2000},
-// 			"created_by":      map[string]interface{}{"string": "Golang"},
-// 			"is_valid":        map[string]interface{}{"boolean": true},
-// 			"explicit_string": map[string]interface{}{"string": "explicit"},
-// 		},
-// 	)
-// 	if err != nil {
-// 		t.Fatalf("CreatePod failed: %v", err)
-// 	}
-// 	if p == nil {
-// 		t.Fatalf("Pod is nil")
-// 	}
-// 	if len(j) == 0 {
-// 		t.Fatalf("JSONPOD is empty")
-// 	}
-// 	fmt.Println("JSONPOD", j)
-
-// 	expectedJSON := "{\"entries\":{\"created_by\":{\"string\":\"Golang\"},\"explicit_string\":{\"string\":\"explicit\"},\"hello\":{\"string\":\"world\"},\"is_valid\":{\"boolean\":true},\"year\":{\"int\":2000}},\"signature\":\"t7+VVUbi7qqSc0bzNemuD8MzLPPlSt+k29H/qVbYYBRXg8bJk4SAWMLFeA2UlcRJWH4N34Ovxs3oLp0OmzVTAg\",\"signerPublicKey\":\"xDP3ppa3qjpSJO+zmTuvDM2eku7O4MKaP2yCCKnoHZ4\"}"
-// 	if j != expectedJSON {
-// 		t.Fatalf("JSONPOD does not match expected.\nExpected: %s\nGot: %s", expectedJSON, j)
-// 	}
-// }
-
 // func TestVerify(t *testing.T) {
 // 	p, _, err := CreatePod(
 // 		"0001020304050607080900010203040506070809000102030405060708090001",
@@ -125,32 +97,13 @@ func TestCreateGoPod(t *testing.T) {
 		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8,
 		9, 0, 1,
 	}
-	startTime := time.Now()
-	Init()
-	elapsed := time.Since(startTime)
-	fmt.Println()
-	fmt.Println("================================================")
-	fmt.Println()
-	fmt.Println("INIT TIME", elapsed)
-
-	startTime = time.Now()
-	_, _, err := CreatePod("0001020304050607080900010203040506070809000102030405060708090001", map[string]interface{}{"A": map[string]interface{}{"int": 123}, "B": map[string]interface{}{"int": 321}, "G": map[string]interface{}{"int": 7}, "D": map[string]interface{}{"string": "foobar"}, "C": map[string]interface{}{"boolean": false}})
-	elapsed = time.Since(startTime)
-	fmt.Println("RUST GO CREATE TIME", elapsed)
-	if err != nil {
-		t.Fatalf("CreatePod failed: %v", err)
-	}
-
-	startTime = time.Now()
-	pod, err := CreateGoPod(privKey, PodEntries{
+	pod, err := signPod(privKey, PodEntries{
 		"A": PodValue{kind: "int", intVal: 123},
 		"B": PodValue{kind: "int", intVal: 321},
 		"G": PodValue{kind: "int", intVal: -7},
 		"D": PodValue{kind: "string", strVal: "foobar"},
 		"C": PodValue{kind: "boolean", boolVal: false},
 	})
-	elapsed = time.Since(startTime)
-	fmt.Println("NATIVE GO CREATE TIME", elapsed)
 	if err != nil {
 		t.Fatalf("CreateGoPod failed: %v", err)
 	}
