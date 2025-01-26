9d5e5a7ac9b7170503df91369d829b96007fd2b7
Richard Liu <richard.y.liu@berkeley.edu>
2025-01-26 00:25:41 -0800

    cleanup

--- go/pod/verify.go
+++ go/pod/verify.go
@@ -6,27 +6,7 @@ import (
 	"github.com/iden3/go-iden3-crypto/v2/babyjub"
 )
 
-// Verify uses dispatchRustCommand to check the Pod's signature.
 func (p *Pod) Verify() (bool, error) {
-	// For decoding from unpadded base64
-	// podCopy := *p
-
-	// if len(podCopy.SignerPublicKey) == 64 {
-	// 	rawSPK, err := hex.DecodeString(podCopy.SignerPublicKey)
-	// 	if err != nil {
-	// 		return false, fmt.Errorf("failed to decode signerPublicKey: %w", err)
-	// 	}
-	// 	podCopy.SignerPublicKey = noPadB64.EncodeToString(rawSPK)
-	// }
-
-	// if len(podCopy.Signature) == 128 {
-	// 	rawSig, err := hex.DecodeString(podCopy.Signature)
-	// 	if err != nil {
-	// 		return false, fmt.Errorf("failed to decode signature hex: %w", err)
-	// 	}
-	// 	podCopy.Signature = noPadB64.EncodeToString(rawSig)
-	// }
-
 	contentID, err := computeContentID(p.Entries)
 	if err != nil {
 		return false, fmt.Errorf("failed computing content ID: %w", err)
