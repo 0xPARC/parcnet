f4fed3e30ff2c586fb7681d25cef2e3f5a126bce
Richard Liu <richard.y.liu@berkeley.edu>
2025-01-28 22:04:42 -0800

    feat: ensure legacy format serialization (tested)

--- go/pod/verify.go
+++ go/pod/verify.go
@@ -1,19 +1,44 @@
 package pod
 
 import (
+	"encoding/hex"
 	"fmt"
 
 	"github.com/iden3/go-iden3-crypto/v2/babyjub"
 )
 
 func (p *Pod) Verify() (bool, error) {
+	// Ensure Signature is in hexadecimal format
+	signatureHex := p.Signature
+	if len(p.Signature) != 128 {
+		signatureBytes, err := noPadB64.DecodeString(p.Signature)
+		if err != nil {
+			return false, fmt.Errorf("failed to decode signature from base64: %w", err)
+		}
+		signatureHex = hex.EncodeToString(signatureBytes)
+	}
+
+	// Ensure SignerPublicKey is in hexadecimal format
+	publicKeyHex := p.SignerPublicKey
+	if len(p.SignerPublicKey) != 64 {
+		publicKeyBytes, err := noPadB64.DecodeString(p.SignerPublicKey)
+		if err != nil {
+			return false, fmt.Errorf("failed to decode signer public key from base64: %w", err)
+		}
+		publicKeyHex = hex.EncodeToString(publicKeyBytes)
+	}
+
+	// TODO: remove this
+	// fmt.Println("signatureHex", signatureHex)
+	// fmt.Println("publicKeyHex", publicKeyHex)
+
 	contentID, err := computeContentID(p.Entries)
 	if err != nil {
 		return false, fmt.Errorf("failed computing content ID: %w", err)
 	}
 
 	var sigComp babyjub.SignatureComp
-	if err := sigComp.UnmarshalText([]byte(p.Signature)); err != nil {
+	if err := sigComp.UnmarshalText([]byte(signatureHex)); err != nil {
 		return false, fmt.Errorf("failed to decode signature hex: %w", err)
 	}
 	signature, err := sigComp.Decompress()
@@ -22,7 +47,7 @@ func (p *Pod) Verify() (bool, error) {
 	}
 
 	var publicKey babyjub.PublicKey
-	if err := publicKey.UnmarshalText([]byte(p.SignerPublicKey)); err != nil {
+	if err := publicKey.UnmarshalText([]byte(publicKeyHex)); err != nil {
 		return false, fmt.Errorf("failed to unmarshal public key: %w", err)
 	}
 	err = publicKey.VerifyPoseidon(contentID, signature)
